31aa5549430f4a16695a2fe2264929e9501ed5fd
Marcin Brumer <brumek31@gmail.com>
2018-09-17 16:50:43 +0200

    Added showAlert and clearAlert functions

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,31 @@ class UI {
   constructor() {
     this.profile = document.getElementById("profile");
   }
+
+  showAlert(message) {
+    // Clear remaining alerts
+    this.clearAlert();
+    // Create div
+    const div = document.createElement("div");
+    // Add classes
+    div.className = "alert alert-dismissible alert-danger";
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const parent = document.querySelector(".searchContainer");
+    // Get searchbox
+    const searchBox = document.querySelector(".search");
+    // Insert alert
+    parent.insertBefore(div, searchBox);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
 }
 
 export const ui = new UI();
